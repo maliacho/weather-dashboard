132fa8d31de536eeda79207f0a787f65f5ff32b4
maliacho <maliacho21@gmail.com>
2023-09-05 17:51:45 -0700

    updated HTML file to contain divs for each weather item needed. began to edit CSS file with font-family. added beginnings of psuedocode to JS file

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// Need to define
+    // current weather
+    // future weather
+    // user search input (city name)
+    // API used
+
+// User searches for a city
+    // Current weather conditions displayed
+        // Appropriate Icons related to current weather display
+        // Temperature, Humidity, Wind Index
+    // Future weather conditions displayed
+        // Appropriate Icons for future weather display
+        // Temperature, Humidity, Wind Index
+// Search History is saved
+    // Displayed on screen until User refreshes page
